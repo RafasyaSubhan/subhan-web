8dd1ec281ff239cdcf98814ac9a4ba270876d44f
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-06-16 18:49:27 +0700

    feat: add hover swap effect on profile image

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -197,11 +197,19 @@ export default function Home() {
           </div>
 
           <div className="order-1 md:order-2 flex justify-center md:justify-end relative">
-            <img 
-              src="/rafa-profile.svg" 
-              alt="Rafasya Muhammad Subhan" 
-              className="w-64 md:w-80 lg:w-[400px] object-contain"
-            />
+            <div className="group relative w-64 md:w-80 lg:w-[400px]">
+              <img 
+                src="/rafa-profile.svg" 
+                alt="Rafasya Muhammad Subhan" 
+                className="w-full object-contain transition-all duration-500 group-hover:opacity-0 group-hover:scale-95"
+              />
+
+              <img 
+                src="/rafa-profile2.svg" 
+                alt=""
+                className="w-full object-contain absolute inset-0 opacity-0 transition-all duration-500 group-hover:opacity-100 scale-105 group-hover:scale-100"
+              />
+            </div>
           </div>
         </div>
 
