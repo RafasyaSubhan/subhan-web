cb882e96340f37cb848f08c8ab68385db1a0b81a
RafasyaSubhan <rafasya.subhan@gmail.com>
2026-06-30 14:46:30 +0700

    feat: implement dynamic filtering and empty state

---
+++ frontend/constants/projects.ts
@@ -0,0 +1,29 @@
+export const PROJECTS = [
+  {
+    title: "RISTEK DPD Assignment",
+    description: "Redesigning an outdated university website to highlight the new AI program, focusing on intuitive navigation and visual accessibility for non-tech-savvy users.",
+    image: "/projects/DPD Task.svg",
+    link: "https://www.figma.com/proto/hfnsmaOcuCMM6Nm3QUQEvu/DPD-Tugas-Khusus?node-id=108-264&viewport=128%2C253%2C0.15&t=SEzkjHbQNdrrFNxQ-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=108%3A264&page-id=15%3A3",
+    techStack: ["/logos/figma.svg"],
+    buttonText: "View Prototype",
+    categories: ["UI/UX"] 
+  },
+  {
+    title: "JSON",
+    description: "Built the Auth & Profile service for a proxy shopping platform, featuring secure login, RBAC, traveler KYC, and user moderation.",
+    image: "/projects/JSON Web.svg",
+    link: "https://github.com/advprog-2026-B9-project",
+    techStack: ["/logos/java.svg", "/logos/springboot.svg", "/logos/postgresql.svg", "/logos/supabase.svg", "/logos/typescript.svg", "/logos/css.svg"],
+    buttonText: "View Repository",
+    categories: ["Web Dev"] 
+  },
+  {
+    title: "Suruh-IN",
+    description: "Designing a peer-to-peer errand platform connecting users with freelance runners. The interface streamlines custom daily requests and intuitive task-claiming.",
+    image: "/projects/SuruhIn.svg",
+    link: "https://www.figma.com/proto/q93TTnEABQf0HG48j9cmdp/Suruh-IN-?node-id=245-1425&viewport=1360%2C658%2C0.14&t=L10VvsD82jdJOFVS-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=245%3A1425&show-proto-sidebar=1&page-id=1%3A3",
+    techStack: ["/logos/figma.svg"],
+    buttonText: "View Prototype",
+    categories: ["UI/UX"] 
+  }
+];
